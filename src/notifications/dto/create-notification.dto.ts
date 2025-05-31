9c68707e084a51fb91a06a712e305842585a0ca3
rakhimovcode <abrorbekr11@gmail.com>
2025-05-31 16:43:10 +0500

    new

--- src/notifications/dto/create-notification.dto.ts
+++ src/notifications/dto/create-notification.dto.ts
@@ -1,19 +1,20 @@
-import { IsBoolean, IsNotEmpty, IsString } from "class-validator";
+import { IsBoolean, IsIn, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateNotificationDto {
   @IsNotEmpty()
   @IsString()
-  userId:string
+  userId: string;
 
   @IsNotEmpty()
   @IsString()
+  @IsIn(["streak", "notification", "other"])
   type: string;
 
   @IsNotEmpty()
   @IsString()
   message: string;
 
-   @IsBoolean()
-   @IsNotEmpty()
-   isRead: boolean;
+  @IsBoolean()
+  @IsNotEmpty()
+  isRead: boolean;
 }
